bd49c85352ed5f304b3b634337b97e85f8c78f67
Tiffany Hyatt <tiffany@Hyatt.home>
2017-06-23 19:09:26 -0400

    fixed router

--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -9,9 +9,9 @@ var helpers = {
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         queryURL += '?' + $.param({
             'api-key': nytAPI,
-            'q': article,
-            'begin_date': "",
-            'end_date': "",
+            'q': "Computer",
+            // 'begin_date': "",
+            // 'end_date': "",
             'fl': "headline, web_url"
         });
 
@@ -26,7 +26,7 @@ var helpers = {
 
         return axios.get(queryURL).then(function (response) {
             console.log(response);
-            return response.data.results[0].formatted;
+            // return response.data.results[0].formatted;
         });
     }
 };
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,7 +2,7 @@ var React = require("react");
 var router = require("react-router");
 var Route = router.Route;
 // Include the Router component to contain all our Routes
-var BrowseRouter = require("react-router-dom");
+var Router = router.Router;
 
 var hashHistory = router.hashHistory;
 // Include the IndexRoute (catch-all route)
@@ -14,11 +14,11 @@ var Results = require("..//components/children/Results");
 // Export the Routes
 module.exports = (
   // The high level component is the Router component
-  <BrowseRouter history={hashHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="Search" component={Search} />
         <Route path="Results" component={Results} />
         <IndexRoute component={Main} />
         </Route>
-  </BrowseRouter>
+  </Router>
 );
